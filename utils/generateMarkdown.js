11dd51cce7bc4fffff3a46e19ed0815b6aad39f9
ryanmerced <ryan_merced@hotmail.com>
2022-11-17 21:40:53 -0800

    start to markdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+let readmeBadge = "";
+let readmeLink = "";
+let readmeLicense = "";
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
